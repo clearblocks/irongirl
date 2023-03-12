18480d03e7a8edce2d31c6ff5ef49bb8c1a076bd
Theo van Oostrum <theo@xel.nl>
2023-03-12 15:47:53 +0100

    Smaller images + design changes after review

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import IronTowelsBasket from "../../assets/img/iron-towels-basket.jpg";
 
 export const Footer = () => (
-  <footer><span>&copy;	Copyright 2023 irongirl.nl</span></footer>
+  <footer>
+    <img src={IronTowelsBasket} alt={"strijkijzer met handdoeken en mand"} className={"footer-image"}/>
+    <div className={"footer-container"}>&copy;	Copyright 2023 irongirl.nl</div>
+  </footer>
 );
