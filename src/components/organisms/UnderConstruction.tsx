723d126fc0dfce7c47e3d01e29af594e0add73cc
Theo van Oostrum <theo@xel.nl>
2023-03-13 07:26:11 +0100

    Under construction remove unused import

--- src/components/organisms/UnderConstruction.tsx
+++ src/components/organisms/UnderConstruction.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import UnderConstructionSrc from "../../assets/img/under-construction-icon.png";
 import {ConstructionIcon} from "../atoms/Icon";
+
 export const UnderConstruction = () => (
   <div className={"under-construction"} ><ConstructionIcon/><span>Deze website is momenteel onder constructie. Sommige onderdelen zullen niet werken zoals je zou verwachten.
     Je kunt altijd contact opnemen voor meer informatie over mijn diensten.</span></div>
