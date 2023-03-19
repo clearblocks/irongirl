c8ca3193e0c7d1b3490823362bce78a378e7abd0
Theo van Oostrum <theo@xel.nl>
2023-03-19 14:44:16 +0100

    header logo

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LogoSrc from '../../assets/img/irongirl_logo_transparant.png'
+import LogoSrc from '../../assets/img/irongirl_logo_white.png'
 
 export const Header = () => (
   <header><div><h1>Iron Girl</h1></div><div><img src={LogoSrc} alt={"irongirl logo"}/></div></header>
