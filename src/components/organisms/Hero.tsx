33bd6593c9aab62ad23fcce144c41b147d33f73d
Theo van Oostrum <theo@xel.nl>
2023-04-11 22:54:32 +0200

    Changes in hero text

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => (
     <div>
       <span className={"span-large"}>Strijkservice Vleuten - De Meern</span><br /><br />
       <span>Persoonlijke service</span><br />
-      <span>Snel, voordelig en goed</span><br /><br />
+      <span>Snel, voordelig en zorgvuldig gestreken</span><br /><br />
       <span>Telefoon of WhatsApp:</span><br />
       <span>06-33401173</span>
     </div>
