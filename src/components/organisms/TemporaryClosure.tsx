c6c719943c4ecb8fd53192a455add115d5858f1b
Theo van Oostrum <theo@xel.nl>
2024-02-27 08:47:22 +0100

    fix typo

--- src/components/organisms/TemporaryClosure.tsx
+++ src/components/organisms/TemporaryClosure.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const TemporaryClosure = () => (
   <div className={"temp-close fixed"}>
-    <h3>Wegens zwangerschap zijn wij tijdelijke gesloten. Rond april is de was- en strijkservice weer beschikbaar.</h3>
+    <h3>Wegens zwangerschap zijn wij tijdelijk gesloten. Rond april is de was- en strijkservice weer beschikbaar.</h3>
   </div>
 );
