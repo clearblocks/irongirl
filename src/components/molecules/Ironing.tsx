4e68d43057708efb2d230f7c3b51feb964e2159c
Theo van Oostrum <theovanoostrum@kateinnovations.com>
2024-06-24 21:39:16 +0200

    Added time indication for ironing

--- src/components/molecules/Ironing.tsx
+++ src/components/molecules/Ironing.tsx
@@ -10,6 +10,7 @@ export const Ironing = forwardRef((props, ref: ForwardedRef<HTMLDivElement>) => 
     <ul>
       <li>Het is handig als je zelf kledinghangers kunt afgeven</li>
       <li>Wij zorgen ervoor dat alles perfect netjes gestreken is</li>
+      <li>Binnen 3 tot 5 dagen kun je het strijkgoed weer ophalen</li>
     </ul>
   </div>
 ));
