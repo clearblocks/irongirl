8c6df6f680b240ae28e5661f4986c955d5d0fa3c
Theo van Oostrum <gt.oostrum@gmail.com>
2025-01-01 23:22:03 +0100

    password ignore case

--- public/php/login.php
+++ public/php/login.php
@@ -8,7 +8,7 @@ session_start();
 if ($_SESSION['logged_in'] ?? false) {
     print(json_encode(['logged_in' => true]));
 } else {
-    $loggedIn = ($_POST['password'] ?? null) === 'banaan';
+    $loggedIn = strtolower($_POST['password'] ?? '') === 'banaan';
     $_SESSION['logged_in'] = $loggedIn;
     print(json_encode(['logged_in' => $loggedIn]));
 }
