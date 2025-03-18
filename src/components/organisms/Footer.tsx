1df7f8ce41e074240dbb4aac35e46c8846024b4c
Theo van Oostrum <gt.oostrum@gmail.com>
2025-03-18 23:24:09 +0100

    Increased mobile font-sizes

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -3,6 +3,6 @@ import {GoogleMapsLink} from "../atoms/GoogleMapsLink";
 
 export const Footer = () => (
   <footer>
-    <div className={"footer-container"}><span>&copy;	Copyright 2023 irongirl.nl</span><span>Paddenstoelenlaan 15, 3451 PZ Vleuten</span><GoogleMapsLink /></div>
+    <div className={"footer-container"}><span>&copy;	Copyright 2025 irongirl.nl</span><span>Paddenstoelenlaan 15, 3451 PZ Vleuten</span><GoogleMapsLink /></div>
   </footer>
 );
