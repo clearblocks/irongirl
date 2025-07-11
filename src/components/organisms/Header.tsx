efd3f265d564f303e94ccc9f27e98be5323b8f4c
Theo van Oostrum <gt.oostrum@gmail.com>
2025-07-11 23:40:42 +0200

    Added new logo

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import LogoSrc from '../../assets/img/irongirl_logo_white.png'
+import LogoSrc from '../../assets/img/new-logo-transparent.png'
 import {LanguageSelect} from "../molecules/LanguageSelect";
 
 export const Header = () => (
-  <header><div><h1>Iron Girl</h1></div><div><img src={LogoSrc} alt={"irongirl logo"}/></div>
+  <header><div><img src={LogoSrc} alt={"irongirl logo"}/></div>
     <LanguageSelect /></header>
 );
